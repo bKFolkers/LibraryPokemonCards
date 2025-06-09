b20d86815b20178970b84a535ae8e4805bd7aedd
Bas Folkers <bfolkers07@hotmail.com>
2025-06-09 17:04:16 +0200

    Starter project from Spring initializer

---
+++ src/main/java/nl/miwnn/ch16/bas/librarypokemoncards/LibraryPokemonCardsApplication.java
@@ -0,0 +1,13 @@
+package nl.miwnn.ch16.bas.librarypokemoncards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryPokemonCardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryPokemonCardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/miwnn/ch16/bas/librarypokemoncards/LibraryPokemonCardsApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.miwnn.ch16.bas.librarypokemoncards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryPokemonCardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

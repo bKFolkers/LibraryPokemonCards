961b07ca1a6385b88c2bf18756c364025e642cdf
Bas Folkers <bfolkers07@hotmail.com>
2025-06-10 15:36:59 +0200

    Add the ability to add pokemon cards through a form

--- src/main/java/nl/miwnn/ch16/bas/librarypokemoncards/controller/PokemonCardController.java
+++ src/main/java/nl/miwnn/ch16/bas/librarypokemoncards/controller/PokemonCardController.java
@@ -1,9 +1,13 @@
 package nl.miwnn.ch16.bas.librarypokemoncards.controller;
 
+import nl.miwnn.ch16.bas.librarypokemoncards.model.PokemonCard;
 import nl.miwnn.ch16.bas.librarypokemoncards.repositories.PokemonCardRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -21,11 +25,28 @@ public class PokemonCardController {
         this.pokemonCardRepository = pokemonCardRepository;
     }
 
-    @GetMapping("/")
+    @GetMapping({"/", "/pokemoncard/overview"})
     private String showPokemonCardOverview(Model datamodel) {
         datamodel.addAttribute("allPokemonCards", pokemonCardRepository.findAll());
 
         return "pokemonCardOverview";
     }
 
+    @GetMapping("/pokemoncard/new")
+    private String showNewPokemonCardForm(Model datamodel) {
+        datamodel.addAttribute("formPokemonCard", new PokemonCard());
+        return "pokemonCardForm";
+    }
+
+    @PostMapping("/pokemoncard/save")
+    private String saveOrUpdatePokemonCard(@ModelAttribute("formPokemonCard") PokemonCard pokemonCardToBeSaved,
+                                           BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            System.err.println(bindingResult.getAllErrors());
+        } else {
+            pokemonCardRepository.save(pokemonCardToBeSaved);
+        }
+
+        return "redirect:/pokemoncard/overview";
+    }
 }
